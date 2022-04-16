6162bb6c9c6c7465fd3c7e68616aeec36b39ebe2
Parvesh Narwal <parveshtext@outlook.com>
2022-04-16 17:08:11 +0530

    Add Jwt Authentication at API Gateway level

--- OcelotGateway/Program.cs
+++ OcelotGateway/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +14,35 @@ builder.Host.ConfigureAppConfiguration(
     config => config.AddJsonFile($"ocelot.{env}.json")
     );
 
+//Add Jwt Authentication
+
+var key = "THIS IS PRIVATE SECRET KEY";
+
+builder.Services.AddAuthentication(
+    option =>
+    {
+        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 app.UseOcelot().Wait();
+
+app.UseAuthentication();
 
 app.MapGet("/", () => "Hello World!");
 
